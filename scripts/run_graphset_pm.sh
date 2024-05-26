5f9e05bf49c1650d85212e08031983b6ef087bd6
0x0cqq <cqqqwq@outlook.com>
2024-05-27 00:46:12 +0800

    update

--- scripts/run_graphset_pm.sh
+++ scripts/run_graphset_pm.sh
@@ -1,7 +1,7 @@
 GRAPHAUTOTUNE_HOME=/home/cqq/GraphMining/GraphAutoTuner
 GRAPHSET_PATH=/home/cqq/GraphMining/GraphAutoTune
 
-srun -p V100 --gres=gpu:1 $GRAPHSET_PATH/build/bin/gpu_graph /home/cqq/data/livejournal.g 6 011011101110110101011000110000101000
+srun -p V100 --gres=gpu:1 $GRAPHSET_PATH/build/bin/gpu_graph /home/cqq/data/orkut.g 7 0111111101111111011101110100111100011100001100000
 
 # 5 0111110111110111110111110
 
--- scripts/run_pattern_matching.sh
+++ scripts/run_pattern_matching.sh
@@ -1,4 +1,4 @@
-make -j && srun -p Big -w nico2 --gres=gpu:1 ./bin/pm data/livejournal.bin 011011101110110101011000110000101000
+make -j && srun -p Big -w nico2 --gres=gpu:1 ./bin/pm data/orkut.bin 0111111101111111011101110100111100011100001100000
 
 # nsys profile  --stats=true --sample=cpu --trace=cuda,cudnn,cublas,nvtx,osrt,oshmem --cudabacktrace=kernel:1000000,sync:1000000,memory:1000000 --sampling-period=1000000 --wait=all 
 # ncu --call-stack --nvtx --set full --import-source 1 -o report%i 
@@ -9,4 +9,6 @@ make -j && srun -p Big -w nico2 --gres=gpu:1 ./bin/pm data/livejournal.bin 01101
 
 # 011011101110110101011000110000101000
 # 011111101111110111111011111101111110
+
+# 0111111101111111011101110100111100011100001100000
 # srun -p V100 --gres=gpu:1 ./bin/gpu_graph ../../GraphAutoTuner/data/data_graph_20.txt 5 0100110110010110110010100
